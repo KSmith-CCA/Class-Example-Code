e1fc073ab23c977fac1dc1d3780089f0343ef13e
KSmith-CCA <112913803+KSmith-CCA@users.noreply.github.com>
2022-10-11 20:29:24 -0400

    Add files via upload

---
+++ password.py
@@ -0,0 +1,40 @@
+password = input()
+valid = 0
+lower = 0
+upper = 0
+int_count = 0
+invalid_char = 0
+
+for i in range(len(password)):
+    if password[i].islower():
+        lower = lower +1
+    elif password[i].isupper():
+        upper = upper +1    
+    elif password[i] >= '0' and password[i] <= '9':
+        int_count = int_count + 1
+    else:
+        invalid_char = invalid_char + 1
+
+
+print('lower = ', lower)
+print('upper = ', upper)
+print('number = ', int_count)
+print('invalid character ', invalid_char)
+
+if len(password) > 7 and len(password) < 13:
+    valid = valid + 1
+if lower > 2:
+    valid = valid + 1
+if upper > 1:
+    valid = valid + 1
+if int_count > 0:
+    valid = valid + 1
+if invalid_char <= 0:
+    valid = valid + 1
+
+print('Valid Count = ', valid)
+
+if valid == 5:
+    print('Valid')
+else:
+    print('Invalid')
